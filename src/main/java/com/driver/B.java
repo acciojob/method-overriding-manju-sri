a9b7b3a6df466e6e027ad5dfed585c23f7d7fb99
Manjula <manjumanohar0704@gmail.com>
2023-03-21 12:49:05 +0530

    successful

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class B extends A {
-
+    public static String meth(){
+        String name = "manju_sri";
+        System.out.println(name);
+        return name;
+    }
 }
