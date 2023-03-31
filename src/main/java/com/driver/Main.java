00e175569a40656f5d03b2e2992e44296bc98021
Manjula <manjumanohar0704@gmail.com>
2023-04-01 00:05:14 +0530

    okay

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
+        B obj1 = new B();
+        obj1.meth();
     }
 }
